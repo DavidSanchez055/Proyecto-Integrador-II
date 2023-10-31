0ec1b1d5bb161a225515112e6d4213c3364f0dd5
ANDRES FLOREZ <andresf.florezg@unac.edu.co>
2023-10-31 14:00:52 -0500

    ragions Model

--- ServerBackend/src/models/RegionsModels.js
+++ ServerBackend/src/models/RegionsModels.js
@@ -5,10 +5,7 @@ const RegionsModels = schema({
     id: String,
     nombre: String,
     img: String,
-    email: String,
-    edad: String,
-    usuario: String,
-    password: String
+    
 });
 
 module.exports = mongoose.model('regions_collection', RegionsModels);
